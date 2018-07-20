3e139159fe4435dd2ff4bcf2faa4e27c07d0e6b0
tky <takayuki.for.public@gmail.com>
2018-07-20 11:45:51 +0900

    Added a feature to test features.

--- test/helper/SpecificationHelper.scala
+++ test/helper/SpecificationHelper.scala
@@ -4,6 +4,7 @@ import play.api.test.Helpers._
 import play.api.test._
 import play.api.mvc._
 import org.scalatest.BeforeAndAfterEach
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play._
 import org.specs2.mutable._
 import org.specs2.mutable.After
@@ -13,7 +14,12 @@ import scala.concurrent.{ Await, Awaitable }
 import scala.concurrent.duration.Duration
 
 // https://www.playframework.com/documentation/2.6.x/api/scala/index.html#play.api.test.PlaySpecification
-trait SpecificationHelper extends PlaySpecification with Results with DomTestHelper with After {
+trait SpecificationHelper
+  extends PlaySpecification
+  with Results
+  with DomTestHelper
+  with After
+  with ScalaFutures {
 
   override def after = EvolutionHelper.clean()
 
